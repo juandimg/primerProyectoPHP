6cb7eec9565d44fa68cc9e2cd283a33ab6081f5c
501 <501@academicos.cesde.edu.co>
2020-02-14 21:05:32 -0500

    mi primer codigo

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+//Esto es un comentario de linea
+
+/*Este es un comentario
+de varias lineas*/
+
+
+$nombre="Juan Diego";
+echo($nombre);
+
+$edad=20;
+echo($edad);
+$estatura=1.76;
+$nombre="Angela";
+echo($nombre);
+
+
+
+$edad=105;
+echo($edad);
+
+echo("Hola mundo");
+
+echo("<br>");
+echo("Mi nombre es ".$nombre." y la edad es".$edad);
+
+
+
+?>
